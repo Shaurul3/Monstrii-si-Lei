13bf802c8b72acb4a1402fec507dd9cbaec90bfb
Shaurul3 <94381820+Shaurul3@users.noreply.github.com>
2023-01-02 21:23:08 +0200

    FullCastWednesday function

--- Monstrii si Lei.cpp
+++ Monstrii si Lei.cpp
@@ -11,10 +11,11 @@ using namespace std;
 //Functia de creeare a fisierului cu figuranti
 void figuranti()
 {
-    remove("Figuranti.csv");
+    remove("Extra.csv");
+
     fstream f1;
 
-    f1.open("Figuranti.csv", ios::out | ios::app);
+    f1.open("Extra.csv", ios::out | ios::app);
 
     srand(time(0));
 
@@ -22,14 +23,40 @@ void figuranti()
     string Prenume[137] = {"Ada","Adela","Alexandra","Marius","Beatrice","Alexandru","Agata","Adina","Aurora","Ana","Corina","Daniela","Ecaterina","Clara","Dana","Elena","Doina","Diana","Ion","Fabiana","Floare","Oana","Iasmina","Ionela","Geta","Francesca","Ina","Lili","Adam","Camil","Cosmin","Codrin","Bernand","Corneliu","Cezar","Ciprian","Adrian","Darius","David","Cazimir","Damian","Gheorghe","Ghita","Gherasim","Iliuta","Lica","Miron","Matei","Marian","Marin","Jean","Laurentiu","Lascar","Mihai","Mihail","Nicusor","Nicolae","Plesu","Petru","Octav","Radu","Rares","Paul","Robert","Sebastian","Sergiu","Relu","Serban","Stefan","Rafael","Rafaela","Raul","Silviu","Zamfir","Zaharia","Vlad","Vladimir","Viorel","Valentin","Teodor","Vasile","Trandafir","Virgil","Vlad","Toma","Traian","Valeriu","Victor","Vincentiu","Valentina","Veronica","Verona","Viorica","Viorela","Veta","Virginia","Simona","Sabina","Sabrina","Sanda","Sandra","Valeria","Teodora","Timea","Sonia","Ramona","Roxana","Raluca","Rebeca","Ramonca","Silvia","Paulina","Paul","Tudora","Stefania","Olga","Olimpia","Otilia","Speranta","Savina","Roberta","Suzana","Steluta","Nina","Nicoleta","Dinu","George","Horia","Grigore","Ionel","Ghita","Georgian","Gelu","Gabriel","Ilie","Madalin","Laurentiu"};
 
     for(int i = 1; i <= 132; i++)
-    f1<<i<<","<<Nume[rand() % 152]<<" "<<Prenume[rand() % 137]<<","<<"extra"<<"\n";
+    f1<<Nume[rand() % 152]<<" "<<Prenume[rand() % 137]<<","<<"extra"<<"\n";
     f1.close();
+}
+
+//Functie de creeare a fisierului cu intreg castul
+void fullCast()
+{
+    remove("FullCastWednesday.csv");
+
+    ifstream file1("wednesdayCast.csv");
+    ifstream file2("Extra.csv");
+
+    ofstream outfile("FullCastWednesday.csv");
+
+    string line;
+    while (getline(file1, line))
+    {
+        outfile << line << endl;
+    }
+
+    while (getline(file2, line))
+    {
+        outfile << line << endl;
+    }
+
+    file1.close();
+    file2.close();
+    outfile.close();
 }
 
 int main()
 {
     figuranti();
-
+    fullCast();
 
     return 0;
 }
